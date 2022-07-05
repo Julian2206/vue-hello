da05c26e3f3d1d5ed45c8ffc8530c576018a511f
Julian2206 <julian.pcc991@gmail.com>
2022-07-05 15:24:54 +0200

    ..js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const app = new Vue({
   el: "#root",
   data: {
     content: "CatsAreSupurrrrior",
-    catName: "Begula",
+    catName: "",
     image:
       "https://yt3.ggpht.com/-zmdP1LF5JdZXIvL3-k_OAFmsmHOqb9BR1RukzrTl54JCe_E_Job0qVKzKG9ZIZgrBsGx1K2WA=s900-c-k-c0x00ffffff-no-rj",
   },
